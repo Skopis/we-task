9980e7f188c8fa94fe69bfd0ab2e87f552d3fd2b
ShacharDf <shaars7@gmail.com>
2021-03-17 18:16:07 +0200

    dragable

--- frontend/src/cmps/group-list.vue
+++ frontend/src/cmps/group-list.vue
@@ -1,36 +1,50 @@
-<template> 
-        <ul class="group-list">
-            <li v-for="task in groupToShow.tasks" :key="'C'+task.id">
-                <task-preview :task="task"/>
-                <!-- <router-link :to="'/task/'+task._id"></router-link> -->
-            </li>
-        </ul>
+<template>
+  <!-- <ul class="group-list"> -->
+  <draggable
+    tag="ul"
+    class="group-list"
+    v-bind="dragOptions"
+    :list="groupToShow.tasks"
+  >
+    <li v-for="task in groupToShow.tasks" :key="'C' + task.id">
+      <task-preview :task="task" />
+      <!-- <router-link :to="'/task/'+task._id"></router-link> -->
+    </li>
+  </draggable>
+  <!-- </ul> -->
 </template>
 
 <script>
-import taskDetails from './task-details.vue'
-import taskPreview from './task-preview.vue'
+import taskDetails from "./task-details.vue";
+import taskPreview from "./task-preview.vue";
+import draggable from "vuedraggable";
 
 export default {
-    name: 'group-list',
-    props:['group'],
-    data() {
-        return {
-            groupToShow: null
-        }
+  name: "group-list",
+  props: ["group"],
+  data() {
+    return {
+      groupToShow: null,
+    };
+  },
+  computed: {
+    dragOptions() {
+      return {
+        animation: 200,
+        group: "description",
+        disabled: false,
+        ghostClass: "ghost",
+      };
     },
-    computed: {
-
-    },
-    created() {
-        this.groupToShow = this.group
-    },
-    methods: {
-    
-    },
-    components:{
-        taskDetails,
-        taskPreview
-    }
-}
+  },
+  created() {
+    this.groupToShow = this.group;
+  },
+  methods: {},
+  components: {
+    taskDetails,
+    taskPreview,
+    draggable,
+  },
+};
 </script>
