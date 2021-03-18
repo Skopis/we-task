0b54834d36d018364e0885cbf3c8505860296b82
nufa flufa <nofargabso@gmail.com>
2021-03-18 10:57:30 +0200

    task details modal

--- frontend/src/cmps/task-details.vue
+++ frontend/src/cmps/task-details.vue
@@ -1,13 +1,29 @@
 <template>
-  <div v-if="task" class="task-details">
-    <h1>{{ task.title }}</h1>
-    <h5>in list</h5>
-    <h4>Description</h4>
-    <p>{{ task.description }}</p>
-    <h4>Activities</h4>
-    <div v-if=" activities">
-      <div v-for="activity in activities" :key="activity.id">
-        <task-activities :activity="activity" />
+  <div v-if="task" class="task-details-modal">
+    <div class="task-details-content">
+      <header>
+        <button class="btn close-btn">+</button>
+        <h1>{{ task.title }}</h1>
+        <p><span>in list</span></p>
+      </header>
+      <div class="task-members">
+        <h3>MEMBERS</h3>
+        <div v-for="member in task.members" :key="member._id">
+          <img class="avatar" :src="member.imgUrl" alt="" />
+        </div>
+      </div>
+      <div class="task-desc">
+        <h3>Description</h3>
+        <p v-if="task.description">{{ task.description }}</p>
+        <p v-else class="description-area"> Add a more detailed description </p>
+      </div>
+      <div class="task-activities">
+        <h3>Activities</h3>
+        <div v-if="activities">
+          <div v-for="activity in activities" :key="activity.id">
+            <task-activities :activity="activity" />
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -30,8 +46,8 @@ export default {
         const task = await this.$store.dispatch({ type: "getById", id });
         this.task = task;
         let taskActivities = this.$store.getters.taskActivities;
-        if(!taskActivities || !taskActivities.length) return
-        else this.activities = taskActivities
+        if (!taskActivities || !taskActivities.length) return;
+        else this.activities = taskActivities;
         console.log("active length:", this.activities.length);
       } catch (err) {
         console.log("Cannot find task", err);
