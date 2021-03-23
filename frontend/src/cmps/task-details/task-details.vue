e14fa56f50602d445aa8161715dd5894be1b7095
Lian Skopis <lian.skopis@gmail.com>
2021-03-23 19:11:07 +0200

    sdf

--- frontend/src/cmps/task-details/task-details.vue
+++ frontend/src/cmps/task-details/task-details.vue
@@ -176,8 +176,9 @@ export default {
       this.loadTask()
     },
     async updateDueDate(date){
-      this.task.dueDate = this.formattedDate(date)
-      await this.$store.dispatch({type: 'addTask', task:this.task})
+      var taskToEdit = JSON.parse(JSON.stringify(this.task)) 
+      taskToEdit.dueDate = this.formattedDate(date)
+      await this.$store.dispatch({type: 'addTask', task:taskToEdit})
       this.addActivity('Added Due Date')
     },
     async addActivity(activityType){
@@ -219,7 +220,7 @@ export default {
         allArr = this.activities
       else allArr = this.task.comments
       var sortedArr = allArr.sort((a, b)=>{
-        return a.createdAt - b.createdAt
+        return b.createdAt - a.createdAt
       })
       return sortedArr
     },
