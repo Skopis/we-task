54c1a8848e0cff7e4dc030ed80ef52137f9aef46
nufa flufa <nofargabso@gmail.com>
2021-03-25 14:26:39 +0200

    css for menu modal

--- frontend/src/views/board.vue
+++ frontend/src/views/board.vue
@@ -32,13 +32,15 @@
         <button class="btn" @click="toggleAddMemberModal">Invite</button>
         <add-board-member @addMemberToBoard="addMemberToBoard" v-if="isAddMemberModalOpen"/>
       </div>
-      <div class="flex row-reverse align-center">
-        <button class="btn" @click="toggleBoardMenuModal">
+      <div class="board-menu" :class="{ active: isBoardMenuModalOpen}">
+        <button class="btn  btn-menu-modal" @click="toggleBoardMenuModal" v-if="!isBoardMenuModalOpen">
           <img src="../assets/icons/3dots.png" alt="" />
         </button>
         <board-menu
           v-if="isBoardMenuModalOpen"
+          :classSetting="isBoardMenuModalOpen"
           :board="boardToShow"
+          @closeModal="closeBoardMenuModal"
           @updateBoardCover="updateBoardCover"
           @searchChanged="searchChanged"
         />
@@ -195,6 +197,9 @@ export default {
       //TODO:
       this.isBoardMenuModalOpen = !this.isBoardMenuModalOpen;
     },
+    closeBoardMenuModal(){
+      this.isBoardMenuModalOpen=false
+    },
     archiveGroup(groupToArchive) {
       this.$store.dispatch({
         type: "archiveGroup",
@@ -253,12 +258,13 @@ export default {
       const groupIdx = this.boardToShow.groups.findIndex(
         (group) => group.id === groupId
       );
-      // console.log('groupIdx', groupIdx)
-      var amount = 276 * (groupIdx + 1) - 12;
-      if (groupIdx < 1) {
-        amount -= 12;
+      console.log('groupIdx', groupIdx)
+      var amount = 285 * (groupIdx + 1);
+      if (!groupIdx) {
+        amount -= 10;
       }
-      this.menuPos = { left: amount + "px" };
+      console.log('amount',amount)
+      this.menuPos = { left:amount + "px" };
       // console.log(this.menuPos);
     },
   },
